bb662c5da64147d3798bcd07ed80c15d93127b2a
userHyoin <chiw9123@gmail.com>
2023-01-18 02:05:23 +0900

    Inital commit

--- src/main/java/com/zerock/edureview/entity/Education.java
+++ src/main/java/com/zerock/edureview/entity/Education.java
@@ -1,4 +1,23 @@
 package com.zerock.edureview.entity;
 
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
 public class Education {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long eno;
+
+    private String title;
 }
