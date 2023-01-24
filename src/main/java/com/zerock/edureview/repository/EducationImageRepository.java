f395407e23d8f31fbaa948d058aaea8f81c5fd66
userHyoin <chiw9123@gmail.com>
2023-01-24 17:58:57 +0900

    repository 생성 및 테스트

--- src/main/java/com/zerock/edureview/repository/EducationImageRepository.java
+++ src/main/java/com/zerock/edureview/repository/EducationImageRepository.java
@@ -4,4 +4,5 @@ import com.zerock.edureview.entity.EducationImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EducationImageRepository extends JpaRepository<EducationImage, Long> {
+
 }
