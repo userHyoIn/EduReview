cac33c1b89fdfef83f91b315de83a7c1c3286aa1
userHyoin <chiw9123@gmail.com>
2023-01-25 13:12:02 +0900

    education 평균점수/리뷰개수를 위한 education repository @Quey문 추가 및 테스트

--- src/test/java/com/zerock/edureview/repository/EducationRepositoryTests.java
+++ src/test/java/com/zerock/edureview/repository/EducationRepositoryTests.java
@@ -49,7 +49,7 @@ public class EducationRepositoryTests {
                 educationImageRepository.save(educationImage);
             }
 
-            System.out.println("==================================================");
+            System.out.println("===================================================");
         });
     }
 
