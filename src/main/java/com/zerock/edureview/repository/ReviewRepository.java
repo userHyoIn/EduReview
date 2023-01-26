cba592d42d201f8c87055311f314c39425f8df41
userHyoin <chiw9123@gmail.com>
2023-01-26 20:02:34 +0900

    Image 중간 저장

--- src/main/java/com/zerock/edureview/repository/ReviewRepository.java
+++ src/main/java/com/zerock/edureview/repository/ReviewRepository.java
@@ -1,7 +1,22 @@
 package com.zerock.edureview.repository;
 
+import com.zerock.edureview.entity.Education;
+import com.zerock.edureview.entity.Member;
 import com.zerock.edureview.entity.Review;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
+    List<Review> findByEducation(Education education);
+
+    @Modifying
+    @Query("delete from Review mr where mr.member = :member")
+    void deleteByMember(Member member);
 }
--- src/test/java/com/zerock/edureview/repository/ReviewRepositoryTests.java
+++ src/test/java/com/zerock/edureview/repository/ReviewRepositoryTests.java
@@ -6,11 +6,17 @@ import com.zerock.edureview.entity.Review;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
 public class ReviewRepositoryTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Autowired
     private ReviewRepository reviewRepository;
@@ -37,5 +43,40 @@ public class ReviewRepositoryTests {
 
             reviewRepository.save(educationReview);
         });
+    }
+
+    @Test
+    public void testGetMovieReviews(){
+
+        Education education = Education.builder().eno(94L).build();
+
+        List<Review> result = reviewRepository.findByEducation(education);
+
+        result.forEach(educationReview -> {
+
+            System.out.print(educationReview.getReviewnum());
+            System.out.print("\t" + educationReview.getGrade());
+            System.out.print("\t" + educationReview.getText());
+            System.out.print("\t" + educationReview.getMember().getEmail());
+            System.out.println(" ----------------------------------------------------");
+        });
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteMember(){
+
+        Long eid = 1L;
+
+        Member member = Member.builder().eid(eid).build();
+
+        //기존
+        //memberRepository.deleteById(eid);
+        //reviewRepository.deleteByMember(member);
+
+        //순서 주의
+        reviewRepository.deleteByMember(member);
+        memberRepository.deleteById(eid);
     }
 }
